537cd589440019f6af0f57293ed6f3117ea84e8c
Taus <tausg@hotmail.com>
2019-11-11 21:07:43 +0100

    Added low and high temp and uv index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,14 @@ const forecast = (lat, long, callback) => {
                 temperature : body.currently.temperature,
                 precipProbability : body.currently.precipProbability,
                 summary : body.daily.data[0].summary,
-                textSummary : body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+                temperatureLow : body.daily.data[0].temperatureLow,
+                temperatureHigh : body.daily.data[0].temperatureHigh,
+                uvIndex : body.currently.uvIndex,
+                textSummary : body.daily.data[0].summary +
+                    ' It is currently ' + body.currently.temperature + ' degress out. ' +
+                    'There is a ' + body.currently.precipProbability + '% chance of rain.' +
+                    ' Todays lowest temperature will be ' + body.daily.data[0].temperatureLow + ' and todays high will be ' + body.daily.data[0].temperatureHigh + 
+                    '. The UV index for taday is ' + body.currently.uvIndex
             });
         }
     });
